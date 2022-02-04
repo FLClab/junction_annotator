0cd89ce92c2c4f057235192ae5044d2f27de420f
reber199 <renaud.bernatchez.1@ulaval.ca>
2022-02-04 13:52:36 -0500

    update roseline

--- loader.py
+++ loader.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 
 class Loader:
-    def __init__(self, path, crop_size=512, crop_step=int(512*0.75)):
+    def __init__(self, path, outputpath=None, crop_size=128, crop_step=int(128*0.75), total_size=256):
         """
         Crop iterator
         :param path:
@@ -12,10 +12,12 @@ class Loader:
         :param crop_step:
         """
         self.path = path
+        self.outputpath= outputpath
         self.files = os.listdir(path)
 
         self.crop_size = crop_size
         self.crop_step = crop_step
+        self.total_size = total_size
 
         self.file_idx = 0
         self.n = 0
@@ -29,6 +31,7 @@ class Loader:
         :return: Image with shape [height, width, color]
         """
         self.image = io.imread(img_path).astype('float32')
+        #print("image ", self.image.shape)
         self.image[0] = self.image[0] - np.min(self.image[0], axis=0)
         self.image[1] = self.image[1] - np.min(self.image[1], axis=0)
         self.image[0] = self.image[0] / np.percentile(self.image[0], 99.9)
@@ -41,8 +44,10 @@ class Loader:
         buffer = self.image[:,:,1].copy()
         self.image[:,:,1] = self.image[:,:,0].copy()
         self.image[:,:,0] = buffer
+        #print("image after ", self.image.shape)
 
     def generate_crops(self, img_path):
+        #print("Generating crop for ", img_path )
         self.load_image(img_path)
         self.crop_data = []
         for j in range(0, self.image.shape[0], self.crop_step):
@@ -55,23 +60,8 @@ class Loader:
         return self
 
     def __next__(self):
-        if self.file_idx >= len(self.files):
-            raise StopIteration
 
-        crop = self.crop_data[self.n]
-        y = crop['Y']
-        x = crop['X']
-        size = crop['size']
-        crop = self.image[y:y+size, x:x+size].copy()
-
-        if crop.shape[0] != crop.shape[1]:
-            canvas = np.zeros((self.crop_size, self.crop_size, 3), dtype='uint8')
-            canvas[:crop.shape[0], :crop.shape[1], :] = crop
-            crop = canvas
-
-        self.n += 1
-
-        if self.n > len(self.crop_data):
+        if self.n >= len(self.crop_data):
             self.file_idx += 1
             self.x = 0
             self.y = 0
@@ -79,11 +69,63 @@ class Loader:
                 self.n = 0
                 self.generate_crops(os.path.join(self.path, self.files[self.file_idx]))
 
+        if self.file_idx >= len(self.files):
+            #print("Stop iteration ")
+            raise StopIteration
+
+
+        crop = self.crop_data[self.n]
+        y = crop['Y']
+        x = crop['X']
+        size = crop['size']
+
+        pad_size = (self.total_size - self.crop_size)//2
+        n_pad = ((pad_size, pad_size), (pad_size, pad_size), (0, 0))
+        image_pad = np.pad(self.image, n_pad, mode='constant')
+
+        crop = image_pad[y-pad_size:y+size+pad_size, x-pad_size:x+size+pad_size].copy()
+        if crop.shape[0] != crop.shape[1]:
+            canvas = np.zeros((self.total_size, self.total_size, 3), dtype='uint8')
+            canvas[:crop.shape[0], :crop.shape[1], :] = crop
+            crop = canvas
+
+        self.n += 1
 
         return crop
 
+    def save_patch(self,image, classes, labelling_time=0, ext=None):
+        print("Image ", image.shape)
+        orig_fname=os.path.join(self.path, self.files[self.file_idx])
+        orig_fname_spplit= os.path.splitext(self.files[self.file_idx])
+        if ext is None:
+            ext=str(orig_fname_spplit[0])
+        io.imsave(os.path.join(self.outputpath, str(orig_fname_spplit[0])+'_'+str(self.n)+'.'+ext), image)
+        with open(os.path.join(self.outputpath,"patchlist.txt"), "a") as file_object:
+            file_object.write(os.path.join(self.outputpath, str(orig_fname_spplit[0])+'_'+str(self.n)+'.'+ext)+";"+str(classes)+";"+labelling_time+"\n")
+
+    def save_crop_data(self, classes, labelling_time):
+        orig_fname= os.path.join(self.path, self.files[self.file_idx])
+        orig_fname_spplit= os.path.splitext(self.files[self.file_idx])
+        if len(self.crop_data)>self.n-1:
+            with open(os.path.join(self.outputpath,"patchlist.txt"), "a") as file_object:
+                file_object.write(orig_fname+";"+str(self.crop_data[self.n-1]['X'])+";"+str(self.crop_data[self.n-1]['Y'])+";"+str(self.crop_data[self.n-1]['size'])+";"+str(classes)+";"+labelling_time+"\n")
+
+def generate_box(crop_size=128, total_size=256):
+    """
+    Generate the focus box in the center
+    """
+    pad_size = (total_size - crop_size) // 2
+    box = np.zeros((total_size,total_size,3)).astype('bool')
+    box[pad_size-3:pad_size+crop_size+3, pad_size-3:pad_size+crop_size+3, (1,2)] = True
+    box[pad_size:pad_size+crop_size, pad_size:pad_size+crop_size, (1,2)] = False
+
+    return box
+
 if __name__=='__main__':
     data_path = 'example_image'
-    loader = Loader(data_path)
+    out_path = 'outdir_image'
+    os.makedirs(out_path, exist_ok=True)
+    loader = Loader(path=data_path, outputpath=out_path)
     for l in loader:
+        loader.save_patch(l, [0,0,2,3], 'png')
         pass
--- main.py
+++ main.py
@@ -2,23 +2,34 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QPixmap, QImage, QKeySequence
 from skimage import io
+#Add
+import os
+#os.chdir("C:/Users/olory/Desktop/PostDoc/ProjetIntestin/junction_annotator-main/")
+#
+
 from gui import Ui_JunctionAnnotator
-from loader import Loader
+from loader import Loader, generate_box
 import numpy as np
 import sys
 import time
 from matplotlib import pyplot as plt
+import math
+import datetime
+#from PyQt5 import QtCore
 
 class App(QMainWindow, Ui_JunctionAnnotator):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
         # Setup variables
-        self.path = self.select_path()
-        self.loader = Loader(path=self.path)
+        self.path = self.select_path(title="Select source path")
+        self.outputpath = self.select_path(title="Select patch destination path")
+        self.loader = Loader(path=self.path, outputpath=self.outputpath)
         self.labels = [False for _ in range(4)]
+        self.labelValues = [0.0 for _ in range(4)]
         self.current_time = time.time()
         self.time_steps = []
+        self.time_steps_calc=[]
 
         # Image settings
         self.zoom_level = np.float32(1.0)
@@ -27,6 +38,7 @@ class App(QMainWindow, Ui_JunctionAnnotator):
         self.dragging = False
 
         # Initial image
+        self.box = generate_box(self.loader.crop_size, self.loader.total_size)
         self.next_crop()
         self.display_crop()
 
@@ -44,26 +56,135 @@ class App(QMainWindow, Ui_JunctionAnnotator):
 
         # Shortcuts/hotkeys
 
-        shortcuts = {}
-        for n, check in enumerate(self.frame_choices.children()):
-            check.clicked.connect(self.update_classification)
-            shortcuts[str(n+1)] = QShortcut(QKeySequence(str(n+1)), self)
-        shortcuts['1'].activated.connect(lambda: self.check_shortcut('1'))
-        shortcuts['2'].activated.connect(lambda: self.check_shortcut('2'))
-        shortcuts['3'].activated.connect(lambda: self.check_shortcut('3'))
-        shortcuts['4'].activated.connect(lambda: self.check_shortcut('4'))
-        QShortcut(QKeySequence('Return'), self).activated.connect(self.submit)
-        QShortcut(QKeySequence('P'), self).activated.connect(self.pause)
+        #shortcuts = {}
+        #for n, check in enumerate(self.frame_choices.children()):
+        #    check.clicked.connect(self.update_classification)
+        #    shortcuts[str(n+1)] = QShortcut(QKeySequence(str(n+1)), self)
+        #shortcuts['1'].activated.connect(lambda: self.check_shortcut('1'))
+        #shortcuts['2'].activated.connect(lambda: self.check_shortcut('2'))
+        #shortcuts['3'].activated.connect(lambda: self.check_shortcut('3'))
+        #shortcuts['4'].activated.connect(lambda: self.check_shortcut('4'))
+        #QShortcut(QKeySequence('Return'), self).activated.connect(self.submit)
+        #QShortcut(QKeySequence('P'), self).activated.connect(self.pause)
+
+        self.slider_class_1.valueChanged.connect(lambda: self.update_class_value(1))
+        self.slider_class_2.valueChanged.connect(lambda: self.update_class_value(2))
+        self.slider_class_3.valueChanged.connect(lambda: self.update_class_value(3))
+        self.slider_class_4.valueChanged.connect(lambda: self.update_class_value(4))
+
+        self.Spin_class_1.valueChanged.connect(lambda: self.update_spin_class_value(1))
+        self.Spin_class_2.valueChanged.connect(lambda: self.update_spin_class_value(2))
+        self.Spin_class_3.valueChanged.connect(lambda: self.update_spin_class_value(3))
+        self.Spin_class_4.valueChanged.connect(lambda: self.update_spin_class_value(4))
+
+        #self.slider_zoom.valueChanged.connect(self.zoom_change)
+
+
+        self.timer = QTimer(self)
+        #self.timer.setGeometry(QtCore.QRect(530, 480, 181, 30))
+        #self.timer.setObjectName("timer")
+        self.button_start_timer.clicked.connect(self.start_action)
+        #self.button_pause_timer.clicked.connect(self.pause_action)
+        self.button_stop_timer.clicked.connect(self.stop_action)
+        # adding action to timer
+        self.timer.timeout.connect(self.showTime)
+        # update the timer every tenth second
+
+        self.start= False
+        self.count = -1
+        self.timer.start(1000)
+
+        self.curr_time = QTime(00,00,00)
+        self.timeEdit_timer.setTime(self.curr_time) #QTime(0, 0, 0))
+        ##self.upTime.setDisplayFormat('hh:mm:ss')
+
+        self.timenow=QDateTime.currentDateTime()
+        #timeDisplay=time.toString('yyyy-MM-dd hh:mm:ss dddd')
+     
+        #self.button_start_timer.setEnabled(True)
+        #self.button_pause_timer.setEnabled(False)
+        #self.button_stop_timer.setEnabled(False)
 
         self.show()
 
-    def select_path(self):
+
+    def reset_class_values(self):
+        self.labelValues = [0.0 for _ in range(4)]
+
+        self.slider_class_1.setValue(0)
+        self.slider_class_2.setValue(0)
+        self.slider_class_3.setValue(0)
+        self.slider_class_4.setValue(0)
+
+        self.Spin_class_1.setValue(0.0)
+        self.Spin_class_2.setValue(0.0)
+        self.Spin_class_3.setValue(0.0)
+        self.Spin_class_4.setValue(0.0)
+
+    def resetZoom(self):
+        self.zoom_level=1.0
+        #self.slider_zoom.setValue(int(self.zoom_level*100))
+        #self.spin_zoom.setValue(self.zoom_level)
+
+
+    def timerUpdateTime(self):
+        #
+        self.curr_time = self.curr_time.addSecs(1)
+        self.timeEdit_timer.setTime(self.curr_time) #setText (self.curr_time.toString("hh:mm:ss"))
+        ##self.timeEdit_timer.setTime(self.upTime.time().addSecs(1))
+
+
+    # method called by timer
+    def showTime(self):
+  
+        # checking if flag is true
+        if self.start:
+            # incrementing the counter
+            #self.count -= 1
+            self.timerUpdateTime()
+  
+            # timer is completed
+            #if self.count == 0:
+  
+                # making flag false
+            #    self.start = False
+  
+            #    # setting text to the label
+            #    self.label_timer.setText("00:00:00")
+  
+        #if self.start:
+        #    # getting text from count
+        #    #text = str(self.count) str(self.count / 10) + " s"
+        #    m, s = divmod(self.count, 60)
+        #    h, m = divmod(m, 60)
+        #    text='{:d}:{:02d}:{:02d}'.format(h, m, s) #str(datetime.timedelta(seconds=self.count))
+        #    # showing text
+        #    self.label_timer.setText(text)
+
+    def select_path(self, title= "Select Directory"):
         """
         Select the directory containing the images to label
         :return:
         """
-        path = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        path = str(QFileDialog.getExistingDirectory(self, title))
         return path
+
+    def display_crop000(self):
+        """
+        Update the displayed crop
+        """
+        self.label_image.clear()
+        self.displayed_crop = self.crop.copy()
+        self.update_contrast()
+        h, w, c = self.displayed_crop.shape
+        img = np.transpose(self.displayed_crop, (1,0,2)).copy()
+        pixmap = QImage(img, w, h, QImage.Format_RGB888)
+        q_pixmap= QPixmap(pixmap)
+        q_size = q_pixmap.size()
+        scaled_pixmap = q_pixmap.scaled(self.zoom_level * q_size)#self.pixmap.scaled(self.scale * size)
+        self.label_image.setPixmap(scaled_pixmap)
+        #scaleImage(self)
+        self.show()
 
     def display_crop(self):
         """
@@ -74,9 +195,38 @@ class App(QMainWindow, Ui_JunctionAnnotator):
         self.update_contrast()
         h, w, c = self.displayed_crop.shape
         img = np.transpose(self.displayed_crop, (1,0,2)).copy()
-        pixmap = QImage(img, w, h, QImage.Format_RGB888)
-        self.label_image.setPixmap(QPixmap(pixmap))
+        #self.scaleImage1(img, h, w, c)
+        #self.scaleImage2(img, h, w, c)
+        self.scaleImage(img, h, w, c)
         self.show()
+
+    def scaleImage1(self, img, h, w, c):
+        pixmap = QImage(img, w, h, QImage.Format_RGB888)
+        q_pixmap= QPixmap(pixmap)
+        q_size = q_pixmap.size()
+        scaled_pixmap = q_pixmap.scaled(self.zoom_level * q_size)#self.pixmap.scaled(self.scale * size)
+        self.label_image.setPixmap(scaled_pixmap)
+
+    def scaleImage2(self, img, h, w, c):
+        pixmap = QImage(img, w, h, QImage.Format_RGB888)
+        q_pixmap = QPixmap(pixmap)
+        self.label_image.setPixmap(q_pixmap.scaled(self.label_image.size()))
+
+
+    #https://gist.github.com/acbetter/32c575803ec361c3e82064e60db4e3e0
+    def scaleImage(self, img, h, w, c):
+        #if self.label_image.pixmap() is None:
+        self.scaleImage2(img, h, w, c)
+        self.label_image.resize(self.zoom_level * self.label_image.pixmap().size())
+
+        self.adjustScrollBar(self.scrollArea.horizontalScrollBar(), self.zoom_level)
+        self.adjustScrollBar(self.scrollArea.verticalScrollBar(), self.zoom_level)
+
+
+    def adjustScrollBar(self, scrollBar, factor):
+        scrollBar.setValue(int(factor * scrollBar.value()
+                               + ((factor - 1) * scrollBar.pageStep() / 2)))
+
 
     def submit(self):
         """
@@ -84,13 +234,22 @@ class App(QMainWindow, Ui_JunctionAnnotator):
         """
         self.time_steps.append(time.time() - self.current_time)
         time_taken = np.sum(self.time_steps)
+        self.time_steps_calc.append(self.curr_time.toString("hh:mm:ss"))
+        #self.loader.save_patch(image=self.crop, classes=self.labelValues, labelling_time=self.curr_time.toString("hh:mm:ss"), ext="png")
+        #self.save_crop_data(classes=self.labelValues, labelling_time=self.time_steps[-1].toString("hh:mm:ss"))
+        self.loader.save_crop_data(classes=self.labelValues, labelling_time=self.curr_time.toString("hh:mm:ss"))
         self.next_crop()
+        self.curr_time =  QTime(00,00,00)
+        self.start_action()
 
     def skip(self):
         """
         Skip the current crop?
         """
         self.next_crop()
+        self.curr_time =  QTime(00,00,00)
+        self.start_action()
+
 
     def next_crop(self):
         """
@@ -98,9 +257,13 @@ class App(QMainWindow, Ui_JunctionAnnotator):
         """
         self.time_steps = []
         self.current_time = time.time()
-        for i, check in enumerate(self.frame_choices.children()):
-            check.setChecked(False)
-            self.labels[i] = False
+        #for i, check in enumerate(self.frame_choices.children()):
+        #    check.setChecked(False)
+        #    self.labels[i] = False
+        
+        self.reset_class_values()
+        self.resetZoom()
+
 
         try:
             self.crop = self.loader.__next__()
@@ -115,22 +278,111 @@ class App(QMainWindow, Ui_JunctionAnnotator):
         self.display_crop()
         self.show()
 
+
+        
+    def start_action(self):
+        # making flag true
+        self.start = True
+        #self.timer.start(1000)
+
+        #self.curr_time =  QTime(00,00,00)
+        self.timeEdit_timer.setTime(self.curr_time) #QTime(0, 0, 0))
+        self.button_start_timer.setEnabled(False)
+        #self.button_pause_timer.setEnabled(True)
+        self.button_stop_timer.setEnabled(True)
+  
+        ## count = 0
+        ##if self.count == 0:
+        ##    self.start = False
+  
+    def pause_action(self):
+  
+        # making flag false
+        self.start = False
+        #self.timer.stop()
+
+        self.button_start_timer.setEnabled(True)
+        #self.button_pause_timer.setEnabled(False)
+        self.button_stop_timer.setEnabled(False)
+  
+    def stop_action(self):
+        # making flag false
+        self.start = False
+        #self.timer.stop()
+  
+        # setting count value to 0
+        self.count = 0
+        #self.label_timer.setText("00:00:00")
+        self.curr_time = QTime(00,00,00)
+        #self.timeEdit_timer.setTime(self.curr_time) #QTime(0, 0, 0))
+        self.button_start_timer.setEnabled(True)
+        #self.button_pause_timer.setEnabled(False)
+        self.button_stop_timer.setEnabled(False)
+        
+    def reset_action(self):
+  
+        # making flag false
+        self.start = False
+        #self.timer.stop()
+  
+        # setting count value to 0
+        self.count = 0
+        self.label_timer.setText("00:00:00")
+        self.curr_time = QTime(00,00,00)
+        self.timeEdit_timer.setTime(self.curr_time) #QTime(0, 0, 0))
+        self.button_start_timer.setEnabled(True)
+        #self.button_pause_timer.setEnabled(False)
+        self.button_stop_timer.setEnabled(False)
+
+
+
+    def update_class_value(self, i):
+        if i==1:
+            self.labelValues[0]= self.slider_class_1.value()/100
+            self.Spin_class_1.setValue(self.labelValues[0])#setProperty("value", self.labelValues[0])
+        if i==2:
+            self.labelValues[1]= self.slider_class_2.value()/100
+            self.Spin_class_2.setValue(self.labelValues[1])#setProperty("value", self.labelValues[1])
+        if i==3:
+            self.labelValues[2]= self.slider_class_3.value()/100
+            self.Spin_class_3.setValue(self.labelValues[2])#setProperty("value", self.labelValues[2])
+        if i==4:
+            self.labelValues[3]= self.slider_class_4.value()/100
+            self.Spin_class_4.setValue(self.labelValues[3])#setProperty("value", self.labelValues[3])
+       
+    def update_spin_class_value(self, i):
+        if i==1:
+            self.labelValues[0]= self.Spin_class_1.value()
+            self.slider_class_1.setValue(int(self.labelValues[0]*100))#setProperty("value", self.labelValues[0])
+        if i==2:
+            self.labelValues[1]= self.Spin_class_2.value()
+            self.slider_class_2.setValue(int(self.labelValues[1]*100))# setProperty("value", self.labelValues[1])
+        if i==3:
+            self.labelValues[2]= self.Spin_class_3.value()
+            self.slider_class_3.setValue(int(self.labelValues[2]*100))#setProperty("value", self.labelValues[2])
+        if i==4:
+            self.labelValues[3]= self.Spin_class_4.value()
+            self.slider_class_4.setValue(int(self.labelValues[3]*100))#setProperty("value", self.labelValues[3])
+        
+        
+
+
     def update_classification(self):
         """
         Update current classification vector
         """
-        for i, check in enumerate(self.frame_choices.children()):
-            self.labels[i] = check.isChecked()
+        #for i, check in enumerate(self.frame_choices.children()):
+        #    self.labels[i] = check.isChecked()
 
     def check_shortcut(self, event):
         """
         Shortcut event to check classes using 1,2,3,4
         :param event: key pressed
         """
-        for i, check in enumerate(self.frame_choices.children()):
-            if str(i+1) == event:
-                check.setChecked(not check.isChecked())
-        self.update_classification()
+        #for i, check in enumerate(self.frame_choices.children()):
+        #    if str(i+1) == event:
+        #        check.setChecked(not check.isChecked())
+        #self.update_classification()
 
     def update_contrast(self):
         """
@@ -150,6 +402,7 @@ class App(QMainWindow, Ui_JunctionAnnotator):
         x_min, x_max = max(0, edge + self.drag_x), min(w, w - edge + self.drag_x)
 
         self.displayed_crop = self.displayed_crop.copy()[y_min:y_max, x_min:x_max]
+        self.displayed_crop[self.box] = 255
 
     def zoom(self):
         """
@@ -167,6 +420,16 @@ class App(QMainWindow, Ui_JunctionAnnotator):
             self.zoom_level -= np.float32(0.20)
         if self.zoom_level == 1.0:
             self.drag_x, self.drag_y = 0, 0
+        self.display_crop()
+
+    def zoom_change(self):
+        #self.zoom_level=np.float32(self.slider_zoom.value()/100)
+        #self.spin_zoom.setValue(self.zoom_level)
+        self.display_crop()
+
+    def zoom_change_spin(self):
+        #self.zoom_level=np.float32(self.spin_zoom.value())
+        #self.slider_zoom.setValue(int(self.zoom_level*100))
         self.display_crop()
 
     def image_click(self, event):
@@ -211,6 +474,7 @@ class App(QMainWindow, Ui_JunctionAnnotator):
         self.show()
 
         self.time_steps.append(time.time() - self.current_time)  # Add time step to prevent counting time while paused
+        self.pause_action()
 
         pause_dialog = QMessageBox()
         pause_dialog.setIcon(QMessageBox.Warning)
@@ -221,6 +485,8 @@ class App(QMainWindow, Ui_JunctionAnnotator):
         button.setText('Unpause')
         pause_dialog.exec()
         self.display_crop()
+
+        self.start_action()
 
         self.current_time = time.time()
 
